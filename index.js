5564c01f0f1bbd6c86e6fda24e3856d6e6a975bc
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-12-05 15:12:04 +0100

    database connect

--- index.js
+++ index.js
@@ -1,62 +1,82 @@
 const express = require('express')
+var mysql = require("mysql")
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'api-symfony'
+})
+connection.connect();
 const app = express()
 const port = 3000
-app.route('/users/')
-.get((req, res) => {
-    res.statusCode = 200;
-    res.send("voilà les users")
-})
-.post((req, res)=>{
-    res.statusCode=201
-    res.send("utilisateur créé")
-})
-app.post('/users/connect',(req,res)=>{
-    res.statusCode=204
+app.route('/users')
+    .get((req, res) => {
+        const users = connection.query('SELECT * FROM user', (error, result, fields) => {
+            if (error) throw error;
+            res.send(result)
+        })
+        connection.end()
+        res.statusCode = 200;
+    })
+    .post((req, res) => {
+        res.statusCode = 201
+        res.send("utilisateur créé")
+    })
+app.post('/users/connect', (req, res) => {
+    res.statusCode = 204
     res.send("utilisateur connecté")
 })
-app.get('/groupes',(req,res)=>{
+app.get('/groupes', (req, res) => {
+    const groupes = connection.query('SELECT name FROM groupe', (error, result, fields) => {
+        if (error) throw error;
+        res.send(result)
+        connection.end()
+    })
     res.statusCode = 200;
-    res.send("voilà les groupes")
 })
-app.get('/groupes/users',(req,res)=>{
+app.get('/groupes/users', (req, res) => {
+    const groupes = connection.query('SELECT groupe.name, user.firstName, user.lastname FROM groupe INNER JOIN user on user.groupe_id=groupe.id', (error, result, fields) => {
+        if (error) throw error;
+        res.send(result)
+        connection.end()
+    })
     res.statusCode = 200;
-    res.send("voilà le machup")
 })
 app.route('/user/id')
-.get((req,res)=>{
-    res.statusCode = 200;
-    res.send("voilà les detail")
-})
-.patch((req,res)=>{
-    res.statusCode = 204;
-    res.send("Modif effectués")
-})
-app.patch('/user/id/groupe',(req,res)=>{
+    .get((req, res) => {
+        res.statusCode = 200;
+        res.send("voilà les detail")
+    })
+    .patch((req, res) => {
+        res.statusCode = 204;
+        res.send("Modif effectués")
+    })
+app.patch('/user/id/groupe', (req, res) => {
     res.statusCode = 204;
     res.send("ajout de groupe")
 })
-app.patch('/admin/user/id',(req,res)=>{
+app.patch('/admin/user/id', (req, res) => {
     res.statusCode = 204;
     res.send("Modif effectués")
 })
-app.delete('/admin/user/id',(req,res)=>{
+app.delete('/admin/user/id', (req, res) => {
     res.statusCode = 204;
     res.send("user effacé")
 })
-app.post('/admin/groupe',(req,res)=>{
-    res.statusCode=201
+app.post('/admin/groupe', (req, res) => {
+    res.statusCode = 201
     res.send("groupe créé")
 })
-app.patch('/admin/groupe/id',(req,res)=>{
-    res.statusCode=201
+app.patch('/admin/groupe/id', (req, res) => {
+    res.statusCode = 201
     res.send("groupe modifié")
 })
-app.delete('/admin/groupe/id',(req,res)=>{
-    res.statusCode=201
+app.delete('/admin/groupe/id', (req, res) => {
+    res.statusCode = 201
     res.send("groupe supprimé")
 })
-app.patch('/admin/groupe/id/users',(req,res)=>{
-    res.statusCode=201
+app.patch('/admin/groupe/id/users', (req, res) => {
+    res.statusCode = 201
     res.send("utilisateurs modifié")
 })
 app.listen(port, () => {
